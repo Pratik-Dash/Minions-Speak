00e61b85f4913b6660ef3fa5708e02eefb1d44bb
Pratik Dash <dashpratik40@gmail.com>
2022-08-08 01:19:36 +0530

    Added error handling messages.
    Factor- Refactored the code and added error handling code.

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ var output_textarea = document.querySelector("#output")
 var translate_btn = document.querySelector("#btn-translate")
 var reset_btn = document.querySelector("#btn-reset")
 const url = "https://api.funtranslations.com/translate/minion.json"
-
-
+var requestUrl = ""
+var input = ""
+function constructRequestURL(userInput){
+    input = userInput
+    requestUrl = url+"?text="+input
+}
+function errorHandler(error)
+{
+    console.log("Number of request exceeded, please try after an hour",error)
+}
 translate_btn.addEventListener("click",function getText()
 {
-    var input = input_textarea.value
-    fetch(url+"?text="+input)
+    var userInput = input_textarea.value
+    constructRequestURL(userInput)
+    fetch(requestUrl)
     .then(response => response.json())
     .then(data => document.querySelector("#output").innerText = data.contents.translated)
+    .catch(errorHandler);
 })
 reset_btn.addEventListener("click",(function clear(){
 
